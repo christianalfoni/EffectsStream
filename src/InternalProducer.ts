83faef78149ba0b51e899c21aa62df426c993b13
Christian Alfoni <christianalfoni@gmail.com>
2018-03-04 09:27:44 +0100

    fix fork typing... almost

--- src/InternalProducer.ts
+++ src/InternalProducer.ts
@@ -1,96 +1,133 @@
-import { Executor, Execution } from './Executor'
-import { throwError } from './utils'
+import { Executor, Execution } from './Executor';
+import { throwError } from './utils';
 
-export type NextCallback<Input, Context> = (value: Input, context: Context, execution: Execution) => void
-export type ErrorCallback<Context> = (value: Error, context: Context, execution: Execution) => void
-export type CompleteCallback<Context> = (value: any, context: Context, execution: Execution) => void
+export type NextCallback<Input, Context> = (value: Input, context: Context, execution: Execution) => void;
+export type ErrorCallback<Context> = (value: Error, context: Context, execution: Execution) => void;
+export type CompleteCallback<Context> = (value: any, context: Context, execution: Execution) => void;
 
 export type Listener<Input, Context> = {
-  next: NextCallback<Input, Context>
-  error: ErrorCallback<Context>
-  complete: CompleteCallback<Context>
-}
-export type Listeners<Input, Context> = Listener<Input, Context>[]
+	next: NextCallback<Input, Context>;
+	error: ErrorCallback<Context>;
+	complete: CompleteCallback<Context>;
+};
+export type Listeners<Input, Context> = Listener<Input, Context>[];
+
+type InternalProducerFactory = (input: InternalProducer<any, any>) => InternalProducer<any, any>;
+
+type Callback<InputProducer, OutputProducer> = (p: InputProducer) => OutputProducer;
+
+type BasePaths<Paths> = { [Path in keyof Paths]: Paths[Path] };
 
 export class InternalProducer<Input, Context> {
-  _listeners: Listeners<Input, Context> = []
-  _context?: Context
-  _executor?: Executor
-  map<Output>(callback: (value: Input, context: Context) => Output) {
-    const producer = new InternalProducer<Output, Context>()
-    this.subscribe((value: Input, context: Context, execution: Execution) => {
-      const result: Output | Promise<Output> = callback(value, context)
-      if (result instanceof Promise) {
-        result.then((value) => {
-          producer.next(value, context, execution)
-        }).catch(producer.error.bind(producer))
-      } else {
-        producer.next(result, context, execution)
-      }
-    }, throwError, () => {})
+	_listeners: Listeners<Input, Context> = [];
+	_context?: Context;
+	_executor?: Executor;
+	valueType: Input;
+	fork<Paths extends BasePaths<Paths>, OutputPath>(
+		callback: (value: Input, context: Context) => OutputPath | Promise<OutputPath> & keyof Paths,
+		paths: Paths
+	) {
+		let producer = new InternalProducer<ReturnType<Paths[OutputPath]>['valueType'], Context>();
+		this.subscribe((value: Input, context: Context, execution: Execution) => {
+			const result = callback(value, context);
+			if (result instanceof Promise) {
+				result.then((path) => {
+					const pathProducer = new InternalProducer<Input, Context>();
+					const pathStreamCallback = paths[path];
+					const pathStream = pathStreamCallback(pathProducer);
+					pathStream.subscribe((pathValue, context, execution) => {
+						producer.next(pathValue, context, execution);
+					});
+					pathProducer.next(value, context, execution);
+				});
+			} else {
+				const pathProducer = new InternalProducer<Input, Context>();
+				const pathStreamCallback = paths[result];
+				const pathStream = pathStreamCallback(pathProducer);
+				pathStream.subscribe((pathValue, context, execution) => {
+					producer.next(pathValue, context, execution);
+				});
+				pathProducer.next(value, context, execution);
+			}
+		}, throwError);
+		return producer;
+	}
+	map<Output>(callback: (value: Input, context: Context) => Output) {
+		const producer = new InternalProducer<Output, Context>();
+		this.subscribe(
+			(value: Input, context: Context, execution: Execution) => {
+				const result: Output | Promise<Output> = callback(value, context);
+				if (result instanceof Promise) {
+					result
+						.then((value) => {
+							producer.next(value, context, execution);
+						})
+						.catch(producer.error.bind(producer));
+				} else {
+					producer.next(result, context, execution);
+				}
+			},
+			throwError,
+			() => {}
+		);
 
-    return producer
-  }
-  /*
-  compose(callback) {
-    const producer = new InternalProducer()
-    this.subscribe((value, context, execution) => {
-      producer.next(value, context, execution)
-    }, throwError)
+		return producer;
+	}
+	compose<Output>(callback: (producer: InternalProducer<Input, Context>) => InternalProducer<Output, Context>) {
+		const producer = new InternalProducer<Input, Context>();
+		this.subscribe(
+			(value: Input, context: Context, execution: Execution) => {
+				producer.next(value, context, execution);
+			},
+			throwError,
+			() => {}
+		);
 
-    return callback(producer)
-  }
-  forEach(callback) {
-    const producer = new InternalProducer()
-    this.subscribe((value, context, execution) => {
-      const result = callback(value)
-      if (result instanceof Promise) {
-        result.then(() => {
-          producer.next(value, context, execution)
-        }).catch(producer.error.bind(producer))
-      } else {
-        producer.next(value, context, execution)
-      }
-    }, throwError)
+		return callback(producer);
+	}
+	forEach(callback: (value: Input, context: Context) => void | Promise<void>) {
+		const producer = new InternalProducer<Input, Context>();
+		this.subscribe(
+			(value: Input, context: Context, execution: Execution) => {
+				const result = callback(value, context);
+				if (result instanceof Promise) {
+					result
+						.then(() => {
+							producer.next(value, context, execution);
+						})
+						.catch(producer.error.bind(producer));
+				} else {
+					producer.next(value, context, execution);
+				}
+			},
+			throwError,
+			() => {}
+		);
 
-    return producer   
-  }
-  filter(callback) {
-    const producer = new InternalProducer()
-    this.subscribe((value, context, execution) => {
-      const result = callback(value)
-      if (result instanceof Promise) {
-        result.then((value) => {
-          producer.next(value, context, execution)
-        }).catch(producer.error.bind(producer))
-      } else {
-        producer.next(result, context, execution)
-      }
-    }, throwError)
-    return producer
-  }
-  fork(callback, paths) {
-    const producer = new InternalProducer()
-    this.subscribe((value, context, execution) => {
-      const result = callback(value)
-      if (result instanceof Promise) {
-        result.then((path) => {
-          const pathProducer = new InternalProducer()
-          paths[path](pathProducer).subscribe((pathValue, context, execution) => {
-            producer.next(pathValue, context, execution)
-          })
-          pathProducer.next(value, context, execution)
-        })
-      } else {
-        const pathProducer = new InternalProducer()
-        paths[result](pathProducer).subscribe((pathValue, context, execution) => {
-          producer.next(pathValue, context, execution)
-        })
-        pathProducer.next(value, context, execution)
-      }
-    }, throwError)
-    return producer
-  }
+		return producer;
+	}
+	filter(callback: (value: Input, context: Context) => boolean | Promise<boolean>) {
+		const producer = new InternalProducer<Input, Context>();
+		this.subscribe(
+			(value: Input, context: Context, execution: Execution) => {
+				const result = callback(value, context);
+				if (result instanceof Promise) {
+					result
+						.then((shouldContinue) => {
+							shouldContinue && producer.next(value, context, execution);
+						})
+						.catch(producer.error.bind(producer));
+				} else {
+					result && producer.next(value, context, execution);
+				}
+			},
+			throwError,
+			() => {}
+		);
+
+		return producer;
+	}
+	/*
   catch (callback) {
     const producer = new InternalProducer()
     this.subscribe((value, context, execution) => {
@@ -115,26 +152,29 @@ export class InternalProducer<Input, Context> {
     return producer
   }
   */
-  subscribe(next: NextCallback<Input, Context>, error?: ErrorCallback<Context>, complete?: CompleteCallback<Context>) {
-    this._listeners.push({ next, error, complete })
-  }
-  next(value: Input, context?: Context, execution?: Execution) {
-    this._listeners.forEach((listener) => {
-      listener.next(value, context || this._context, execution || this._executor.create())
-    })
-  }
-  error(error: Error, context?: Context, execution?: Execution) {
-    this._listeners.forEach((listener) => {
-      listener.error(error, context || this._context, execution || this._executor.create())
-    })
-  }
-  complete(value, context?: Context, execution?: Execution) {
-    this._listeners.forEach((listener) => {
-      listener.complete(value, context || this._context, execution || this._executor.create())
-    })
-  }
-  bind(value) {
-    return this.next.bind(this, value)
-  }
+	subscribe(
+		next: NextCallback<Input, Context>,
+		error?: ErrorCallback<Context>,
+		complete?: CompleteCallback<Context>
+	) {
+		this._listeners.push({ next, error, complete });
+	}
+	next(value: Input, context?: Context, execution?: Execution) {
+		this._listeners.forEach((listener) => {
+			listener.next(value, context || this._context, execution || this._executor.create());
+		});
+	}
+	error(error: Error, context?: Context, execution?: Execution) {
+		this._listeners.forEach((listener) => {
+			listener.error(error, context || this._context, execution || this._executor.create());
+		});
+	}
+	complete(value, context?: Context, execution?: Execution) {
+		this._listeners.forEach((listener) => {
+			listener.complete(value, context || this._context, execution || this._executor.create());
+		});
+	}
+	bind(value) {
+		return this.next.bind(this, value);
+	}
 }
-
--- src/operators.spec.ts
+++ src/operators.spec.ts
@@ -1,20 +1,94 @@
 import { Producer } from './Producer';
-import { Execution } from './Executor'
+import { Execution } from './Executor';
 import { expect } from 'chai';
 import 'mocha';
+import { InternalProducer } from './InternalProducer';
 
-describe('Producer', () => {
-  it('should run MAP operator', () => {
-    let subscribeCalled = false
-    const p = new Producer<string, { foo: string }>({ foo: 'bar' })
-    p.map((value, context) => {
-      return 123
-    }).subscribe((value, context, execution) => {
-      subscribeCalled = true
-      expect(context).to.deep.equal({ foo: 'bar' })
-      expect(execution).to.be.instanceOf(Execution)
-    })
-    p.next('foo')
-    expect(subscribeCalled).to.be.ok
-  });
-})
+type Context = { foo: string };
+
+describe('Operators', () => {
+	it('should run MAP operator', () => {
+		let subscribeCalled = false;
+		const p = new Producer<string, Context>({ foo: 'bar' });
+		p
+			.map((value, context) => {
+				expect(value).to.equal('foo');
+				expect(context).to.deep.equal({ foo: 'bar' });
+				return 123;
+			})
+			.subscribe((value) => {
+				subscribeCalled = true;
+				expect(value).to.equal(123);
+			});
+		p.next('foo');
+		expect(subscribeCalled).to.be.ok;
+	});
+	it('should run COMPOSE operator', () => {
+		let subscribeCalled = false;
+		const p = new Producer<string>();
+
+		p.compose((p) => p.map((value) => 123)).subscribe((value) => {
+			subscribeCalled = true;
+			expect(value).to.equal(123);
+		});
+		p.next('foo');
+		expect(subscribeCalled).to.be.ok;
+	});
+	it('should run FOREACH operator', () => {
+		let subscribeCalled = false;
+		const p = new Producer<string, Context>({ foo: 'bar' });
+
+		p
+			.forEach((value, context) => {
+				expect(context).to.deep.equal({ foo: 'bar' });
+				expect(value).to.equal('foo');
+			})
+			.subscribe((value, context) => {
+				subscribeCalled = true;
+				expect(value).to.equal('foo');
+				expect(context).to.deep.equal({ foo: 'bar' });
+			});
+		p.next('foo');
+		expect(subscribeCalled).to.be.ok;
+	});
+	it('should run FILTER operator', () => {
+		let subscribeCalled = false;
+		const p = new Producer<string, Context>({ foo: 'bar' });
+
+		p
+			.filter((value, context) => {
+				expect(context).to.deep.equal({ foo: 'bar' });
+				expect(value).to.equal('foo');
+
+				return true;
+			})
+			.filter((value) => {
+				expect(value).to.equal('foo');
+
+				return false;
+			})
+			.subscribe((value, context) => {
+				subscribeCalled = true;
+				expect(context).to.deep.equal({ foo: 'bar' });
+			});
+		p.next('foo');
+		expect(subscribeCalled).to.not.be.ok;
+	});
+	it('should run FORK operator', () => {
+		let subscribeCalled = false;
+		const p = new Producer<string>();
+
+		p
+			.fork(() => 'bar', {
+				foo: (p: Producer<string>) => p.map((value) => 'foo'),
+				bar: (p: Producer<string>) => p.map((value) => 123)
+			})
+			.subscribe((value, context) => {
+				subscribeCalled = true;
+				expect(value).to.equal(123);
+			});
+
+		p.next('foo');
+		expect(subscribeCalled).to.be.ok;
+	});
+});
